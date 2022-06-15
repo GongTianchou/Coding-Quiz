e84b52d3e6009c98823b4406f8f2fdfe21c2132e
Tianchou Gong <tianchougong@gmail.com>
2022-06-15 14:35:56 -0400

    css and questions

---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+var timeEL = document.getElementById('Timeleft');
+var startEl = document.getElementById('start');
+var answerEl = document.getElementsByClassName('answers')
+var backEl = document.getElementById('back');
+var clearEL = document.getElementById('clear');
+var HighscoreEL = document.getElementById('Highscore');
+
+
+
+
+
+
+
+
+var question = ["1.what does HTML stands for?",
+"2.Which of the following characters indicate closing of a tag?",
+"3.How many attributes are there in HTML5?",
+"4.Which of the following attributes is used to open an hyperlink in new tab?"]
+var answerA =["Hypertext Machine language",".","2","tab"]
+var answerB =["Hypertext and links markup language","/","4","href"]
+var answerC =["Hypertext Markup Language","//","1","target"]
+var answerD =["Hightext machine language","!","5","ref"]
+var correctAnswer = ["C","B","A","C"]
